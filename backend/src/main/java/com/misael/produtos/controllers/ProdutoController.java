d98db51336841ea76afb3c75150b26de6e618b44
Misael Borges <mizaelborges2011@gmail.com>
2022-12-15 13:17:38 -0300

    update class ProdutoController

--- backend/src/main/java/com/misael/produtos/controllers/ProdutoController.java
+++ backend/src/main/java/com/misael/produtos/controllers/ProdutoController.java
@@ -2,44 +2,50 @@ package com.misael.produtos.controllers;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.misael.produtos.entities.Produto;
 import com.misael.produtos.services.ProdutoService;
 
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
     
     @Autowired
     private ProdutoService service;
 
     @GetMapping("/list")
-    public List<Produto> list() {
-        return service.list();
+    public ResponseEntity<List<Produto>> list() {
+        return ResponseEntity.status(200).body(service.list());
     }
 
     @GetMapping("/list/{id}")
-    public Optional<Produto> findById(Long id) {
-        return service.listById(id);
+    public ResponseEntity<Optional<Produto>> findById(@PathVariable Long id) {
+        return ResponseEntity.status(200).body(service.listById(id));
     }
 
     @PostMapping("/save")
-    public Produto save(Produto produto) {
-        return service.save(produto);
+    public ResponseEntity<Produto> save(@RequestBody Produto produto) {
+        return ResponseEntity.status(201).body(service.save(produto));
     }
 
-    @PutMapping("edit")
-    public Produto ediProduto(Produto produto) {
-        return service.edit(produto);
+    @PutMapping("/edit")
+    public ResponseEntity<Produto> ediProduto(@RequestBody Produto produto) {
+        return ResponseEntity.status(201).body(service.edit(produto));
     }
 
-    @DeleteMapping("/delete")
-    public Boolean delete(Long id) {
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
-        return true;
+        return ResponseEntity.status(204).build();
     }
 }
--- backend/src/main/java/com/misael/produtos/repositories/ProdutoRepository.java
+++ backend/src/main/java/com/misael/produtos/repositories/ProdutoRepository.java
@@ -1,10 +1,8 @@
 package com.misael.produtos.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.misael.produtos.entities.Produto;
 
-@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     
 }
