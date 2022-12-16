0d0de52e25f81412178cb6416e8a994c58cc178c
Misael Borges <mizaelborges2011@gmail.com>
2022-12-16 14:49:12 -0300

    create class Formulario.js e Tabela.js

---
+++ frontend/src/Tabela.js
@@ -0,0 +1,23 @@
+function Tabela() {
+    return (
+        <table className="table">
+            <thead>
+                <th>Código-Produto</th>
+                <th>Nome</th>
+                <th>Marca</th>
+                <th>Preço</th>
+                <th>Selecionar</th>
+            </thead>
+
+            <tbody>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+        </table>
+    )
+}
+
+export default Tabela;
